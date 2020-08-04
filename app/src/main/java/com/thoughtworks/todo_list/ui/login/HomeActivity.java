8acc0b347ef38be3763fd9d74bedf1282bfac2ee
fujian.xu <fujian.xu@thoughtworks.com>
2020-08-04 23:30:26 +0800

    home页面的toolbar基本布局

--- app/src/main/java/com/thoughtworks/todo_list/ui/login/HomeActivity.java
+++ app/src/main/java/com/thoughtworks/todo_list/ui/login/HomeActivity.java
@@ -3,14 +3,20 @@ package com.thoughtworks.todo_list.ui.login;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.thoughtworks.todo_list.R;
+
+import java.util.Objects;
 
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activiey_home);
+        setContentView(R.layout.activity_home);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
     }
 }
